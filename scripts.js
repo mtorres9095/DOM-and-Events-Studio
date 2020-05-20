3d415d3147f153abe4137c7a683cc09d9602be93
Miguel <mtorres9095@gmail.com>
2020-05-20 10:53:37 -0400

    completed up, down, right, left and work. Thanks Moe

--- scripts.js
+++ scripts.js
@@ -11,6 +11,9 @@ let paragraph = null;
 let currentFligthStatus = null;
 let shuttleFlightScreen = null;
 let shuttleHeight = 0;
+let rocket = null;
+
+
 
 
 
@@ -30,15 +33,24 @@ function init(){
     downArrow = document.getElementById("downarrow");
     leftArrow = document.getElementById("leftarrow");
     rightArrow = document.getElementById("rightarrow");
+    rocketModel = document.getElementById("rocket");
+    rocketModel.style.position = 'absolute';
+    rocketModel.style.left =  "0px";
+    rocketModel.style.bottom =  "0px";
+   
 
+    // Takeoff Button function
     takeOffButton.addEventListener("click", function(){
         window.alert("just clicked take off");
         window.confirm("Confirm that the shuttle is ready for takeoff.");
         console.log(takeOffButton);
            currentFligthStatus.innerHTML = "Shuttle in flight."; 
            shuttleFlightScreen.style.background = "blue";
-           shuttleHeight.innerHTML = this.innerHTML + 10000;
+           shuttleHeight.innerHTML = 10000;
+        //    shuttleHeight.innerHTML += 10000; add the Take off or up, etc value of innerHTML
     });
+
+    // 
     landingButton.addEventListener("click", function(){
         window.alert("The shuttle is landing. Landing gear engaged.");
         currentFligthStatus.innerHTML = "The shuttle has landed.";
@@ -52,21 +64,45 @@ function init(){
         shuttleHeight.innerHTML = "0";
     });
     upArrow.addEventListener("click",function(){
-        window.alert("Confirm that you touch the up arrow"); 
-        shuttleHeight.innerHTML = this.innerHTML + 10000;
+        // window.alert("Confirm that you touch the up arrow"); 
+        // window.alert(rocketModel.innerHTML); 
+        let distFromBottom = (parseInt(rocketModel.style.bottom) + 10) +"px";
+        rocketModel.style.bottom = distFromBottom;
+        // rocketModel.style.up = 10;
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) + 10000;
+        // rocket. = 10px
 
     });
     downArrow.addEventListener("click",function(){
-        window.alert("Confirm that you touch the up arrow"); 
-        shuttleHeight.innerHTML = this.innerHTML - 10000;
-
+        //  window.alert("Confirm that you touch the up arrow"); 
+        // window.alert(rocketModel.innerHTML); 
+        let distFromBottom = (parseInt(rocketModel.style.bottom) - 10) +"px";
+        rocketModel.style.bottom = distFromBottom;
+        shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) - 10000;
+        // rocketModel.style.up = 10;
+        // shuttleHeight.innerHTML = this.innerHTML + 10000;
+        // rocket. = 10px
     });
     leftArrow.addEventListener("click",function(){
-        window.alert("Confirm that you touch the up arrow"); 
+        // window.alert("Confirm that you touch the left arrow");
+        //  window.alert("Confirm that you touch the up arrow"); 
+        // window.alert(rocketModel.innerHTML); 
+        let distFromLeft = (parseInt(rocketModel.style.left) - 10) +"px";
+        rocketModel.style.left = distFromLeft;
+        // rocketModel.style.up = 10;
+        // shuttleHeight.innerHTML = this.innerHTML + 10000;
+        // rocket. = 10px 
 
     });
     rightArrow.addEventListener("click",function(){
-        window.alert("Confirm that you touch the up arrow"); 
+        // window.alert("Confirm that you touch the left arrow");
+        //  window.alert("Confirm that you touch the up arrow"); 
+        // window.alert(rocketModel.innerHTML); 
+        let distFromLeft = (parseInt(rocketModel.style.left) + 10) +"px";
+        rocketModel.style.left = distFromLeft;
+        // rocketModel.style.up = 10;
+        // shuttleHeight.innerHTML = this.innerHTML - 10000;
+        // rocket. = 10px 
 
     });
     
